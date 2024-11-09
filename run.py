74417eebadd968a240c9f8f1e402ea6d46d4209d
Andres N. Briones <andres@briones.ch>
2024-11-09 15:04:51 +0100

    Auto openning of the broser

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
 from app import create_app
+import webbrowser
+from threading import Timer
 
 app = create_app()
 
+def open_browser():
+    """Open the default web browser at the Flask application's URL."""
+    webbrowser.open_new('http://127.0.0.1:5000/')
+
 if __name__ == '__main__':
+   # Start the browser after a short delay to ensure the server is running
+    Timer(1, open_browser).start()
     app.run(debug=True)
