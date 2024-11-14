7fda2d89f2f9972e45d54c6cdc4d85faccc23664
Andres N. Briones <andres@briones.ch>
2024-11-14 11:10:25 +0100

    Update for correct datetime format in OHH objects

--- blueprints/statistics.py
+++ blueprints/statistics.py
@@ -38,7 +38,7 @@ def player_stats_plot():
 def generate_cummulative_profit_plot(player_name, db_path, window_size = 10):
     player_profits = get_player_profit_historique(player_name, db_path)
     profits_df =  pd.DataFrame(player_profits)
-    profits_df["date_time"] =  pd.to_datetime(profits_df['date_time'], format='%Y-%m-%d %H:%M:%S')
+    profits_df["date_time"] =  pd.to_datetime(profits_df['date_time'], format='%Y-%m-%dT%H:%M:%SZ')
     profits_df.sort_values(by='date_time', inplace=True, ignore_index=True)
     profits_df["profit_cum"] = np.cumsum(profits_df["profit"])
     profits_df["profit_cum_smooth"] = profits_df["profit_cum"].rolling(window=window_size).mean()
